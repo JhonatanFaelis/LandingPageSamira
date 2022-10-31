253d0f34362146dfec753d694fbb918034b85422
JhonatanFaelis <dev.jhonatan.faelis@gmail.com>
2022-10-31 19:15:15 -0300

    add mrv e js

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ control.addEventListener('click', () =>{
 });    
 });
 
+/*JAVAO para os botoes2*/
+const control2 = document.querySelectorAll('.control2');
+let currentItem2 = 0;
+const elemento2 = document.querySelectorAll('.elemento2');
+const maxItems2 = elemento2.length;
+
+control2.forEach(control2 => {
+control2.addEventListener('click', () =>{
+    const isLeft2 = control2.classList.contains2('arrow-left2');
+        if (isLeft2){
+            currentItem2 -=1;
+        } else {
+            currentItem2 += 1;
+        }
+
+     if (currentItem2 >= maxItems2)   {
+        currentItem2 = 0;
+     }
+
+     if (currentItem2 < 0){
+        currentItem2 = maxItems2 - 1;
+     }
+
+     elemento2.forEach(elemento2 => elemento2.classList.remove('current-element2'));
+
+     elemento2[currentItem2].scrollIntoView({inline : "center", behavior:"smooth"});
+
+     elemento2[currentItem2].classList.add("current-element2");
+});    
+});
